83c1850b0e99d6f8489a369cb9e0a8d3498c69ce
Andre Soares <andregsilv@gmail.com>
2022-04-26 19:26:06 -0300

    inline typed

---
+++ typedmatchers/generate.go
@@ -0,0 +1,4 @@
+package typedmatchers
+
+//// go install github.com/golang/mock/mockgen@v1.6.0
+//go:generate mockgen -package=mocks -destination=internal/mocks/matcher.go github.com/mniak/gomock-contrib/typedmatchers Matcher[any]
