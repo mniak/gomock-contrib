fcc844959ac35e173f9b0b549b53ae965bb7aa08
Andre Soares <andregsilv@gmail.com>
2022-04-27 17:30:24 -0300

    FieldJSON (#12)

--- typedmatchers/field_extensions.go
+++ typedmatchers/field_extensions.go
@@ -6,6 +6,10 @@ func FieldEqual[T any, F any](fieldName string, expectedValue F) fieldMatcher[T,
 	return FieldGeneric[T](fieldName, gomock.Eq(expectedValue))
 }
 
+func FieldJSON[T any, J any](fieldName string, expected map[string]any) any {
+	return Field[T, string](fieldName, JSON(expected))
+}
+
 func FieldInlineJSON[T any, J any](fieldName string, function func(x J) bool) any {
 	return Field[T, string](fieldName, InlineJSON(function))
 }
