7256bfe684b1e751d119f2642ba201e7eed92d44
Andre Soares <andregsilv@gmail.com>
2022-04-29 09:46:19 -0300

    disable typed

--- matchers/typed_inline.go
+++ matchers/typed_inline.go
@@ -1,8 +1,8 @@
 package matchers
 
-import "github.com/mniak/gomock-contrib/typedmatchers"
+// import "github.com/mniak/gomock-contrib/typedmatchers"
 
-func TypedInline[T any](message string, function func(x T) bool) typedMatcher[T] {
-	inlineMatcher := typedmatchers.Inline(message, function)
-	return Typed[T](inlineMatcher)
-}
+// func TypedInline[T any](message string, function func(x T) bool) typedMatcher[T] {
+// 	inlineMatcher := typedmatchers.Inline(message, function)
+// 	return Typed[T](inlineMatcher)
+// }
